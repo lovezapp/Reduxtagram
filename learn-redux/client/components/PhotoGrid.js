1d6a02c2c605962446c889d4d0a6c1e1944242f7
Jamie Love <Jamie.Love@charter.com>
2019-11-30 17:12:01 -0700

    set up core components for photo grid and add react router

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return(
+      <div className="photo-grid">I'm the PhotoGrid.</div>
+    )
+  }
+});
+
+export default PhotoGrid;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,28 @@
-// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// Import CSS
+import css from './styles/style.styl';
+
+// Import Components 
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// Import React Router dependencies
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:photoId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(
+  router,
+  document.getElementById('root')
+);
